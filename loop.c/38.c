32c929e7df0ec0b4d2e68c52ff9dee24a3c83e74
kushsahu380-commits <kushsahu380@gmail.com>
2025-10-31 22:47:54 +0530

    38.c commit

--- loop.c/38.c
+++ loop.c/38.c
@@ -1,11 +1,24 @@
 // write a program to print all the armstrong numbers between 1 and 500.
 #include <stdio.h>
+#include <conio.h>
 int main()
 {
-    for (int i = 1; i <= 100; i++)
+    int x, n, y, z;
+    for (int i = 1; i <= 500; i++)
     {
-        
+        n = i;
+        z = 0;
+        while (n != 0)
+        {
+            y = n % 10;
+            x = y * y * y;
+            z = z + x;
+            n = n / 10;
+        }
+        if (z == i)
+        {
+            printf("\t%d", i);
+        }
     }
-
     return 0;
 }

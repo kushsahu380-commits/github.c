e1febe2a0668161743fcb9de94355289209e95c2
kushsahu380-commits <kushsahu380@gmail.com>
2025-11-03 20:28:42 +0530

    42.c

--- loop.c/42.c
+++ loop.c/42.c
@@ -1,9 +1,25 @@
-//To check wheater the number is palindrome. 
-#include<stdio.h>
-int main(){
-    int x,y;
-    printf("E");
-
-
+// To check wheater the number is palindrome.
+#include <stdio.h>
+int main()
+{
+    int x, y, rev, original;
+    rev = 0;
+    printf("Enter the number");
+    scanf("%d", &x);
+    original = x;
+    while (x != 0)
+    {
+        y = x % 10;
+        rev = rev * 10 + y;
+        x = x / 10;
+    }
+    if (original == rev)
+    {
+        printf("%d is the palindrome", original);
+    }
+    else
+    {
+        printf("%d is not the palindrome", original);
+    }
     return 0;
 }

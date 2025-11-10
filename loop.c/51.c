aee2b79d59609a6f88d3833fe3d77fa7ff0885c6
kushsahu380-commits <kushsahu380@gmail.com>
2025-11-10 14:49:37 +0530

    51.c

--- loop.c/51.c
+++ loop.c/51.c
@@ -1,16 +1,16 @@
 // Print the pattern .
-//  A B C D
-//  A B C D
-//  A B C D
-//  A B C D
+// A
+// A B
+// A B C
+// A B C D
 #include <stdio.h>
 #include <conio.h>
 int main()
 {
     char A, B, C, D;
-    for (int i = 1; i <= 4; i++)
+    for (int i = 65; i <= 68; i++)
     {
-        for (int j = 65; j <= 68; j++)
+        for (int j = 65; j <= i; j++)
         {
             printf("%c ", j);
         }

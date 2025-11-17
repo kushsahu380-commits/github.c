3d04dcae2742a2e83c4c3f9c2f4fbb733ebbbf62
kushsahu380-commits <kushsahu380@gmail.com>
2025-11-17 11:09:52 +0530

    60.c

---
+++ loop.c/60.c
@@ -0,0 +1,22 @@
+//   ****
+//  ****
+// ****
+//****
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= 4 - i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= 4; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}

3d46f059bbecdbff85bd06611db71bbb27bdc941
kushsahu380-commits <kushsahu380@gmail.com>
2025-11-19 16:15:19 +0530

    jai hind

--- loop.c/62.c
+++ loop.c/62.c
@@ -6,20 +6,19 @@
 #include <conio.h>
 int main()
 {
-    int j = 1;
-    for (int i = 1; i <= 7; i++)
+     int n;
+    n = 1;
+    for (int i = 1; i <= 4; i++)
     {
-        for (j = 1; j <= (7 - i) / 2; j++)
+        for (int j = 1; j <= 4 - i; j++)
         {
             printf(" ");
         }
-        if (i % 2 == 1)
+        for (int k = 1; k <= n; k++)
         {
-            for (int k = 1; k <= i; k++)
-            {
-                printf("*");
-            }
+            printf("*");
         }
+        n = n + 2;
         printf("\n");
     }
     return 0;

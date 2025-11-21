cca13030c343a0f5e013adc096ff43e83162c3f6
kushsahu380-commits <kushsahu380@gmail.com>
2025-11-21 12:03:43 +0530

    2.c

--- pointers and functions/2.c
+++ pointers and functions/2.c
@@ -1,1 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
+void england()
+{
+    printf("I am from england \n");
+    return;
+}
+void india()
+{
+    printf("I am from india \n");
+    england();
+    return;
+}
+void australia()
+{
+    printf("I am from australia\n");
+    india();
+    return;
+}
+int main()
+{
+    australia();
+    return 0;
+}

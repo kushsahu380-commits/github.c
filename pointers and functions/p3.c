c4a62186b7644710f94d8af7571de38d65a78091
kushsahu380-commits <kushsahu380@gmail.com>
2025-11-27 13:16:48 +0530

    factorialfun

---
+++ pointers and functions/p3.c
@@ -0,0 +1,25 @@
+// Write a program to print the factorial of first 'n' numbers.
+#include <stdio.h>
+int factorial(int i)
+{
+    int y = 1;
+    for (int j = 1; j <= i; j++)
+    {
+        y = y * j;
+    }
+    return y;
+}
+int main()
+
+{
+    int n, z;
+    printf("Enter first number :\n");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        z = factorial(i);
+        printf("%d! = %d", i, z);
+        printf("\n");
+    }
+    return 0;
+}

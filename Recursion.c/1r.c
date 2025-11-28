569056a5a4f039600057cb8d67b440970db15585
kushsahu380-commits <kushsahu380@gmail.com>
2025-11-28 11:37:35 +0530

    recursion1.c

---
+++ Recursion.c/1r.c
@@ -0,0 +1,20 @@
+// Write a program to make function of a factorial using recursion.
+#include <stdio.h>
+int factorial(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    int recAns = n * factorial(n - 1);
+    return recAns;
+}
+int main()
+{
+    int n;
+    printf("Enter a number :");
+    scanf("%d", &n);
+    int fact = factorial(n);
+    printf(" %d! = %d ", n, fact);
+    return 0;
+}

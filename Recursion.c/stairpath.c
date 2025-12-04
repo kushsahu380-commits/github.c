04e52b824fc55badc6c5dbc8debaa71cbdbddaa7
kushsahu380-commits <kushsahu380@gmail.com>
2025-12-04 10:46:59 +0530

    stairpath.c

---
+++ Recursion.c/stairpath.c
@@ -0,0 +1,20 @@
+// Stair path .
+#include <stdio.h>
+int stair(int n)
+{
+    if (n == 1 || n == 2 )
+    {
+        return n;
+    }
+    int totalways = stair(n - 1) + stair(n - 2);
+    return totalways;
+}
+int main()
+{
+    int n;
+    printf("Enter n ");
+    scanf("%d", &n);
+    int ways = stair(n);
+    printf("%d", ways);
+    return 0;
+}

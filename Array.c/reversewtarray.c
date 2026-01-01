f2c79410123cacbe6c69110f3db1f0dd4cc38254
kushsahu380-commits <kushsahu380@gmail.com>
2026-01-01 10:39:34 +0530

    reverse.without.array

---
+++ Array.c/reversewtarray.c
@@ -0,0 +1,26 @@
+// Write a program to reverse the array without using any extra array .
+#include <stdio.h>
+void swaap(int arr[])
+{
+    int i = 0, j = 6, temp;
+    while (i <= j)
+    {
+        temp = arr[i];
+        arr[i] = arr[j]; // sawpping inside while loop .
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+    return;
+}
+int main()
+{
+    int arr[7] = {1, 2, 3, 4, 5, 6, 7};
+    swaap(arr);
+    printf("REVERSED ARRAY:");
+    for (int k = 0; k <= 6; k++)
+    {
+        printf("%d ", arr[k]);
+    }
+    return 0;
+}

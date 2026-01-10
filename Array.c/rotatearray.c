e2f2c8db071e24702c1a1af547ed0eabdf89e9b1
kushsahu380-commits <kushsahu380@gmail.com>
2026-01-10 10:38:42 +0530

    rotate array k times

--- Array.c/rotatearray.c
+++ Array.c/rotatearray.c
@@ -1,29 +1,38 @@
 // Rotate the given array 'a' by k steps , where k is non-negative
 // note: k can be greater then n as well where n is the size of array 'a'.
 #include <stdio.h>
-void reverse(int arr[], int si, int ei)
+void rotate(int arr[], int fi, int li)
 {
     int temp;
-    for (int i = si, j = ei; i < j; i++, j--) // for reversing the whole array and specific part of an array .
+    for (int i = fi, j = li; i < j; i++, j--)
     {
         temp = arr[i];
-        arr[i] = arr[j];
+        arr[i] = arr[j]; // to swaap the elements of our array .
         arr[j] = temp;
     }
     return;
 }
 int main()
 {
-    int k, n = 7, arr[7] = {1, 2, 3, 4, 5, 6, 7};
-    printf("Enter no of steps :");
+    int n, k;
+    printf("NO OF TIMES ARRAY WILL ROTATE : ");
     scanf("%d", &k);
+    printf("ENTER THE SIZE OF ARRAY : "); // array ka size define ho gaya .
+    scanf("%d", &n);
+    int arr[n];
     k = k % n;
-    reverse(arr, 0, n - 1);
-    reverse(arr, 0, k - 1);
-    reverse(arr, k, n - 1);
-    for (int i = 0; i <= 6; i++)
+    for (int i = 0; i <= n - 1; i++)
     {
-        printf("%d ", arr[i]);
+        printf("ENTER THE ELEMENT OF AN ARRAY FOR INDEX %d ", i);
+        scanf("%d", &arr[i]); // To fill the elements in the array .
+    }
+    rotate(arr, 0, n - 1);
+    rotate(arr, 0, k - 1);
+    rotate(arr, k, n - 1);
+    printf("AFTER ROTATING THE ARRAY %d TIMES : ", k);
+    for (int i = 0; i <= n - 1; i++)
+    {
+        printf("%d ", arr[i]); // TO PRINT OUR ROTATED ARRAY .
     }
     return 0;
 }

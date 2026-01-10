3b3ee2dcb5fe46d6d893823cfc336cc293992526
kushsahu380-commits <kushsahu380@gmail.com>
2026-01-10 20:04:02 +0530

    2D ARRAY INPUT

--- 2-D Array.c/2Darrayinput.c
+++ 2-D Array.c/2Darrayinput.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 int main()
 {
-    int arr[4][3];
-    for (int i = 0; i < 4; i++)
+    int r, c;
+    printf("ENTER THE NUMBER OF ROWS IN OUR 2D ARRAY : ");
+    scanf("%d", &r);
+    printf("ENTER THE NUMBER OF COLUMNS IN OUR 2D ARRAY : ");
+    scanf("%d", &c);
+    int arr[r][c];
+    for (int i = 0; i < r; i++)
     {
-        for (int j = 0; j < 3; j++)
+
+        for (int j = 0; j < c; j++)
         {
             printf("ENTER THE ELEMENT OF AN 2D ARRAY OF ROW %d AND COLOUMN %d : ", i, j);
             scanf("%d", &arr[i][j]);
         }
     }
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < r; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < c; j++)
         {
             printf("%d ", arr[i][j]);
         }

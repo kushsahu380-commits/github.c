88b40a0abd453e8f23069009a7e6a109f072aee2
kushsahu380-commits <kushsahu380@gmail.com>
2026-01-10 19:22:18 +0530

    2D ARRAY PRINT OUTOUT

---
+++ 2-D Array.c/2Darrayoutput.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int arr[4][2] = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+    for (int i = 0; i <= 3; i++)
+    {
+        for (int j = 0; j <= 1; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+    }
+    return 0;
+}

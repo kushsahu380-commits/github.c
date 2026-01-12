ca1c5030eeb06ec4424d03b9f0d8a3c903dfdb4d
kushsahu380-commits <kushsahu380@gmail.com>
2026-01-12 20:13:29 +0530

    STORE 10

---
+++ 2-D Array.c/store10.c
@@ -0,0 +1,22 @@
+// WRITE A PROGRAM TO STORE 10 AT EVERY INDEX OF A 2-D MATRIX WITH 5 ROWS AND 5 COLOUMNS .
+#include <stdio.h>
+int main()
+{
+    int arr[5][5];
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            arr[i][j] = 10;
+        }
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
